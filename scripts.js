1d1a0a44ed7cff3bdedbb4ead7e3231b3bcb46b5
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-10-31 10:08:05 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
 const myHead = document.querySelector('.title');
 const zipField = document.querySeletor('#zipField');
 const zipSubmit = document.querySelector('#zipSubmit');
 zipSubmit.addEventListener('click', getWithZip());
 myHead.textContent = 'Cnale';
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
 
 function getWithZip(){
   myHead.innerHTML = "Cnale";

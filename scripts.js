ba9b812ee0bb7c11bfcd4a6223132eaf4b186962
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-10-31 10:06:22 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const myHead = document.querySelector('#title');
+const myHead = document.querySelector('.title');
 const zipField = document.querySeletor('#zipField');
 const zipSubmit = document.querySelector('#zipSubmit');
 zipSubmit.addEventListener('click', getWithZip());

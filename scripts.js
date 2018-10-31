72316017a0327019e051b952cbf17d306ea8faff
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-10-31 10:01:49 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const myHead = document.querySelector('#title');
 const zipField = document.querySeletor('#zipField');
 const zipSubmit = document.querySelector('#zipSubmit');
-zipSubmit.addEventListener('click', getWithZip);
+zipSubmit.addEventListener('click', getWithZip());
 
 function getWithZip(){
   myHead.innerHTML = "Cnale";

d35bfee24ea2d74a2099dfe2158246fbfb485233
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-10-31 10:18:46 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const myHead = document.getElementById('title');
-myHead.innerHTML = "Cnale";
-const zipField = document.querySeletor('#zipField');
+myHead.innerHTML = "Change";
+/*const zipField = document.querySeletor('#zipField');
 const zipSubmit = document.querySelector('#zipSubmit');
 zipSubmit.addEventListener('click', getWithZip());
 
 
 function getWithZip(){
 }
+*/

2898a78286a4ae0409aff3c83534f503c36d9d82
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-10-31 10:05:46 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const myHead = document.querySelector('#title');
 const zipField = document.querySeletor('#zipField');
 const zipSubmit = document.querySelector('#zipSubmit');
 zipSubmit.addEventListener('click', getWithZip());
-myHead.text("Cnale");
+myHead.textContent = 'Cnale';
 
 function getWithZip(){
   myHead.innerHTML = "Cnale";

bf7aeb96ed0615e12281265d31d5ff5db8c32b2a
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-11-02 10:17:11 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,15 +5,6 @@ var request = new XMLHttpRequest();
 
 function getWithZip(){
     zipFieldHead.innerHTML = zipCode.value;
-    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip={zipCode.value},{1}', true);
+    request.open("GET", 'https://api.openweathermap.org/data/2.5/weather?zip={zipCode.value},{1}&APPID={78005268f12b7d461e73208af08b469d}');
     request.send();
-    var data = JSON.parse(this.response);
-
-    data.forEach(weatherAttrib => {
-        console.log(weatheAttrib.title);
-    });
-}
-
-request.onload = function(){
-    
 }
